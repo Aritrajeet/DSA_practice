998ca0a7f42ee4a1b8fcaaac9da05186471e56d6
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-05-28 00:31:51 +0530

    Create Find Duplicate Number.cpp

---
+++ Striver/Day 2 : Array 2/Find Duplicate Number.cpp
@@ -0,0 +1,22 @@
+//Question:
+
+//Solution:Method 1 (Brute) : Sort and duplicate will be consecutive
+//Method 2: Floyd slow fast method (Linked list)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0],fast = nums[0];
+        do {
+            slow = nums[slow];    //slow = slow->next
+            fast = nums[nums[fast]];    //fast = fast->next->next
+        }
+        while(slow != fast);    //when slow and fast meet the cycle in detected i.e. duplicate is there
+        
+        slow = nums[0]; 
+        while(slow != fast){      //now move both slow and fast ptr one step until they meet. Here, they will reach at the duplicate number together
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
