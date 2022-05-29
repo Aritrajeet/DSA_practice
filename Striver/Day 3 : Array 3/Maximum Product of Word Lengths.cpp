cd6e5c598b506403954f249ffed51a9481303daa
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-05-29 13:11:45 +0530

    Create Maximum Product of Word Lengths.cpp

---
+++ Striver/Day 3 : Array 3/Maximum Product of Word Lengths.cpp
@@ -0,0 +1,30 @@
+//Question: https://leetcode.com/problems/maximum-product-of-word-lengths/
+
+//Solution: Brute force - O(n4) TLE :(
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int maxp = 0;
+        sort(words.begin(),words.end());
+        for (auto x: words){
+            cout<<x<<" ";
+        }
+        for (int i=0; i<words.size(); i++){
+            for (int j=i+1; j<words.size(); j++){
+                bool flag = true;
+                for (int m = 0; m<words[i].length(); m++){
+                    for (int n=0; n<words[j].length(); n++){
+                        if (words[i][m] == words[j][n]){
+                            flag = false;
+                        }
+                    }
+                }
+                if (flag){
+                    int temp = words[i].length() * words[j].length();
+                    maxp = max(maxp, temp);
+                }
+            }
+        }
+        return maxp;
+    }
+};
