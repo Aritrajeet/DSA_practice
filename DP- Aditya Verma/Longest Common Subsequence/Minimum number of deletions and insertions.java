356af40ef580b2fcd7937726424d3c24f188ad2b
Abhishek Verma <66030096+Abhi8787@users.noreply.github.com>
2022-06-15 20:48:14 +0530

    Create Minimum number of deletions and insertions.java

---
+++ DP- Aditya Verma/Longest Common Subsequence/Minimum number of deletions and insertions.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class program
+{
+  public static void main(String[] args) throws Exception
+  {
+    Scanner scn = new Scanner(System.in); 
+    String s1 = scn.next();
+    String s2 = scn.next();
+
+    int[][] dp = new int[s1.length()+1][s2.length()+1];
+
+    for(int i=1 ;i<dp.length ; i++)
+    {
+      for(int j=1 ; j<dp[0].length ;j++)
+      {
+         if(s1.charAt(i-1) == s2.charAt(j-1)) 
+           dp[i][j] = 1 + dp[i-1][j-1];
+
+         else
+           dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+      }
+    }
+   
+    System.out.println("No of Deletion  Operation done is : " + (s1.length()-dp[s1.length()][s2.length()]));
+    System.out.println("No of Insertion Operation done is : " + (s2.length()-dp[s1.length()][s2.length()]));
+  }
+}
