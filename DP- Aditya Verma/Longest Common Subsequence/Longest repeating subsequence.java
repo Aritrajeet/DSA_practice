d2bc9f7e988efe8fd70b2c81860a098cc7976938
Abhishek Verma <66030096+Abhi8787@users.noreply.github.com>
2022-06-15 20:46:54 +0530

    Create Longest repeating subsequence.java

---
+++ DP- Aditya Verma/Longest Common Subsequence/Longest repeating subsequence.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+class program
+{
+  public static void main(String[] args) throws Exception
+  {
+    Scanner scn = new Scanner(System.in);
+    String s1 = scn.next();
+    String s2 = "";
+     
+    for(int i=0 ; i<s1.length() ; i++)
+     s2 += s1.charAt(i);
+
+    int[][] dp = new int[s1.length()+1][s2.length()+1];
+
+    for(int i=1 ; i<dp.length ; i++)
+    {
+      for(int j=1 ; j<dp[0].length ;j++)
+      {
+         if(s1.charAt(i-1) == s2.charAt(j-1) && i != j)
+           dp[i][j] = 1 + dp[i-1][j-1];
+
+         else
+           dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+      }
+    }
+
+   String ans = "";
+   
+   int i = dp.length-1;
+   int j = dp[0].length-1;
+
+   while( i>0 && j>0 )
+   {
+      if(s1.charAt(i-1) == s2.charAt(j-1) && i != j)
+      {
+        ans = s1.charAt(i-1) + ans;
+        i--;
+        j--;
+      }
+
+      else
+      {
+         if(dp[i-1][j] > dp[i][j-1])
+           i--;
+         else
+           j--;
+      }
+   }
+
+
+    System.out.println(ans);
+    System.out.println(dp[dp.length-1][dp[0].length-1]);
+
+
+  }
+}
