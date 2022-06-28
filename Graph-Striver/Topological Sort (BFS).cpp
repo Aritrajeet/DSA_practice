e8d10abd1ec70f0d826516fe4e61f0fdded34e5f
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-06-29 00:37:29 +0530

    Create Topological Sort (BFS).cpp

---
+++ Graph-Striver/Topological Sort (BFS).cpp
@@ -0,0 +1,28 @@
+	vector<int> topo(int N, vector<int> adj[]) {
+      queue<int> q; 
+	    vector<int> indegree(N, 0); 
+	    for(int i = 0;i<N;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0;i<N;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    vector<int> ans;
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop(); 
+	        ans.push_back(node);
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    return ans;
+	}
