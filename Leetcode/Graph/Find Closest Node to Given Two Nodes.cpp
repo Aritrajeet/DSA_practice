4805e3a3bd85aafc8ad56af765fd0c8bf10ab9ec
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-08-01 18:11:16 +0530

    Create Find Closest Node to Given Two Nodes.cpp

---
+++ Leetcode/Graph/Find Closest Node to Given Two Nodes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  
+    //2 dfs traversals -> fill 2 vectors. Then get min distance that is common.
+
+    void dfs(int i, int dist, vector<int>& e, vector<int>& memo) {
+        while (i != -1 && memo[i] == -1) {
+            memo[i] = dist++;
+            i = e[i];
+        }
+    }
+    int closestMeetingNode(vector<int>& e, int node1, int node2) {
+        int res = -1, min_dist = INT_MAX;
+        vector<int> m1(e.size(), -1), m2(e.size(), -1);
+        dfs(node1, 0, e, m1);
+        dfs(node2, 0, e, m2);
+        for (int i = 0; i < e.size(); ++i)
+            if (min(m1[i], m2[i]) >= 0 && max(m1[i], m2[i]) < min_dist) {
+                min_dist = max(m1[i], m2[i]);
+                res = i;
+            }
+        return res;
+    }
+};
