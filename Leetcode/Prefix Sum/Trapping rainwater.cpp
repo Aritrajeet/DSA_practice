1a102f5a840e02c8b3e7e949ba47d187b788ba6a
Aritra Chatterjee <55346275+Aritrajeet@users.noreply.github.com>
2022-08-28 16:45:14 +0530

    Create Trapping rainwater.cpp

---
+++ Leetcode/Prefix Sum/Trapping rainwater.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/trapping-rain-water/
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int curr_rain = 0;
+        vector<int>left_max;
+        int l_max = INT_MIN;
+        vector<int>right_max;
+        int r_max = INT_MIN;
+        for (int i=0; i<height.size(); i++){
+            l_max = max(l_max,height[i]);
+            left_max.push_back(l_max);
+        }
+        for (int i=height.size()-1; i>=0; i--){
+            r_max = max(r_max,height[i]);
+            right_max.push_back(r_max);
+        }
+        reverse(right_max.begin(),right_max.end());
+        int ans=0;
+        for (int i=0; i<height.size(); i++){
+            curr_rain = min(left_max[i],right_max[i]) - height[i];
+            ans += curr_rain;
+        }
+        return ans;
+    }
+};
